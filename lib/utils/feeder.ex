dd45ed01a91622cd4ec3e6dc7889510b141fbd80
Loschcode <laurent.schaffner.code@gmail.com>
2016-12-11 01:40:46 +0100

    refactoring feeder

--- lib/utils/feeder.ex
+++ lib/utils/feeder.ex
@@ -2,54 +2,53 @@ defmodule FeedApi.Utils.Feeder do
 
   @doc """
   We try to get the tweets from the timeline of the user
-  It will fetch all the needed datas and return them into an array
+  It will fetch all the needed entry and return them into an array
   """
   def feed! do
     from_twitter!
   end
 
   def from_twitter! do
-
-    tweets = FeedApi.Scrappers.Twitter.dispatch!
-
-    for tweet <- tweets do
-
-      if tweet.link != nil && tweet.message != nil && tweet.message != "" do
-
-        change_data!(FeedApi.Repo.get_by(FeedApi.Link, title: tweet.message), tweet)
-
-      end
-
-    end
-
+    FeedApi.Scrappers.Twitter.dispatch! |> fetch!
   end
 
-  defp change_data!(link, data) do
+  defp fetch!(data) do
+    for entry <- data do
+      if entry |> valid? do
+        change_entry!(FeedApi.Repo.get_by(FeedApi.Link, title: entry.message), entry)
+      end
+    end
+  end
 
-    if link == nil do
+  defp valid?(entry = %{}) do
+    IO.inspect entry
+    case entry do
+      %{link: nil} -> false
+      %{message: nil} -> false
+       %{message: ""} -> false
+       _ -> true
+    end
+  end
 
+  defp change_entry!(link, entry) do
+    if link |> is_nil do
       # let's insert this entry
       FeedApi.Repo.insert! %FeedApi.Link{
-        title: data.message,
+        title: entry.message,
         description: nil,
-        url: data.link,
+        url: entry.link,
         source: "twitter",
-        published_at: data.date
+        published_at: entry.date
       }
-
     else
-
       # let's update this entry
       FeedApi.Repo.update! FeedApi.Link.changeset(link, %{
-        title: data.message,
+        title: entry.message,
         description: nil,
-        url: data.link,
+        url: entry.link,
         source: "twitter",
       })
-
     end
-
   end
-
 
 end
